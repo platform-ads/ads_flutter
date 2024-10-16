8de22698bd895254ea5359679555a688d31d6621
Rodrigo Silva <rodrigoa.work@gmail.com>
2024-10-16 17:17:18 -0300

    remove tests and add run debug

---
+++ apps/ads_flutter_mob/lib/features/home/home_feature.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:home/home.dart';
+
+class HomeFeature extends StatelessWidget {
+  const HomeFeature({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomePage();
+  }
+}
