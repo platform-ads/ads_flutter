c689745d8c0ebf13c30fe4dfb2999cc31cc4eb89
Manoel Amâncio <manoel_amancio@hotmail.com>
2024-10-16 23:31:37 -0300

    created home screen

--- libraries/home/lib/src/presentation/widgets/home_page_web_widget.dart
+++ libraries/home/lib/src/presentation/widgets/home_page_web_widget.dart
@@ -1,3 +1,4 @@
+import 'package:auth_buttons/auth_buttons.dart';
 import 'package:flutter/material.dart';
 
 class HomePageWebWidget extends StatelessWidget {
@@ -5,10 +6,73 @@ class HomePageWebWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Web'),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 10,
+        actions: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Início',
+                  style: TextStyle(color: Colors.white, fontSize: 17),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Grafícos',
+                  style: TextStyle(color: Colors.white, fontSize: 17),
+                ),
+              ),
+              IconButton.outlined(
+                onPressed: () async {
+                  return await _showDialog(context);
+                },
+                icon: const Icon(Icons.link_outlined),
+              ),
+              const SizedBox(
+                width: 30,
+              )
+            ],
+          )
+        ],
+      ),
+      body: const Column(
+        children: [
+          Center(
+            child: Text('Home Web'),
+          ),
+        ],
       ),
     );
+  }
+
+  Future<void> _showDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 20,
+            ),
+            child: AlertDialog(
+              title: const Text('Integrações'),
+              actions: [
+                FacebookAuthButton(
+                  text: 'Integrar com o Facebook ADS',
+                  onPressed: () {},
+                ),
+                GoogleAuthButton(
+                  text: 'Integrar com o Google ADS',
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
