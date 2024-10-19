249b297801f2cdc472e4a4d171ac24bf0a253c43
Crewlan <rodrigoa.work@gmail.com>
2024-10-19 13:30:04 -0300

    feat: add libs in export commons

---
+++ services/commons/lib/commons.dart
@@ -0,0 +1,8 @@
+library commons;
+
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:get_it/get_it.dart';
+export 'package:go_router/go_router.dart';
+export 'package:injectable/injectable.dart';
+export 'package:dio/dio.dart';
+export 'package:equatable/equatable.dart';
