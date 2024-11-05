697063ea6c4b0b59fc2950125e0dc3e2efb6a514
Crewlan <rodrigoa.work@gmail.com>
2024-11-05 20:02:25 -0300

    Update auth_page.dart

--- libraries/home/lib/src/presentation/pages/auth_page.dart
+++ libraries/home/lib/src/presentation/pages/auth_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:core/main.dart';
 import 'package:feature_flag/firebase/firebase_feature_flag.dart';
 import 'package:flutter/material.dart';
